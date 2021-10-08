390f62c3bb760ae9b6c6b5d7309a90e3af98fb08
linh <linhnv296@gmail.com>
2021-10-08 11:29:33 +0700

    db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        $this->call([
+            Database\Seeders\DataTypesTableSeeder::class,
+            Database\Seeders\DataRowsTableSeeder::class,
+            Database\Seeders\MenusTableSeeder::class,
+            Database\Seeders\MenuItemsTableSeeder::class,
+            Database\Seeders\RolesTableSeeder::class,
+            Database\Seeders\PermissionsTableSeeder::class,
+            Database\Seeders\PermissionRoleTableSeeder::class,
+            Database\Seeders\SettingsTableSeeder::class,
+//            Database\Seeders\PagesTableSeeder::class,
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
