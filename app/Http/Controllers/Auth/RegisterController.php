b2aec7439bf04e9e865b7fce95498b4f61bf0be6
linh <linhnv296@gmail.com>
2021-10-08 11:30:08 +0700

    app and config

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use App\User;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\UserRequest;
+use App\Models\Point;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -29,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
@@ -41,33 +44,52 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
-     * @return \App\User
+     * @param array $data
+     * @return
      */
     protected function create(array $data)
     {
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'phone' => $data['phone'],
+            'sex' => $data['sex'],
+            'province' => $data['province'],
+            'education' => $data['education'],
+            'job' => $data['job'] ?? '',
+            'course' => $data['course'],
+            'date_of_birth' => $data['date_of_birth'],
+            'facebook' => $data['facebook'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    protected function addPoint($data, $user)
+    {
+        $point = setting('admin.point_register');
+        $dataUserInvite = User::query()->where("email", $data['email_user'])->get();
+        if (!$dataUserInvite) return;
+        $user->point = $point;
+        $dataUserInvite[0]->point = $dataUserInvite[0]->point + $point;
+        $user->save();
+        $dataUserInvite[0]->save();
+    }
+
+    public function insert(RegisterRequest $request)
+    {
+//        $data = $request->validated();
+        $data = $request->all();
+        if (($data)) {
+            $user = $this->create($data);
+            $this->addPoint($data, $user);
+            session()->flash('success', __('login-logout.register-success'));
+            return redirect('/login')->with('message', __('login-logout.register-success'));
+        } else {
+            return redirect()->back()->withInput();
+        }
     }
 }
